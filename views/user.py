ed188875f9e0fdfe3226741230392534acf7e4f6
Gary Cason <159806009+garycason@users.noreply.github.com>
2024-05-29 14:12:08 -0500

    Revert "Added delete functionality for user table"

--- views/user.py
+++ views/user.py
@@ -1,8 +1,7 @@
-# user.py
+#user.py
 import sqlite3
 import json
 from datetime import datetime
-
 
 def login_user(user):
     # Checks for the user in the database
@@ -12,26 +11,28 @@ def login_user(user):
     #     json string: If the user was found will return valid boolean of True and the user's id as the token
     #                  If the user was not found will return valid boolean False
 
-    with sqlite3.connect("./db.sqlite3") as conn:
+    with sqlite3.connect('./db.sqlite3') as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute(
-            """
+        db_cursor.execute("""
             select id, username
             from Users
             where username = ?
             and password = ?
-        """,
-            (user["username"], user["password"]),
-        )
+        """, (user['username'], user['password']))
 
         user_from_db = db_cursor.fetchone()
 
         if user_from_db is not None:
-            response = {"valid": True, "token": user_from_db["id"]}
+            response = {
+                'valid': True,
+                'token': user_from_db['id']
+            }
         else:
-            response = {"valid": False}
+            response = {
+                'valid': False
+            }
 
         return json.dumps(response)
 
@@ -43,12 +44,11 @@ def create_user(user_request_body):
     # Returns:
     #     json string: Contains the token of the newly created user
 
-    with sqlite3.connect("./db.sqlite3") as conn:
+    with sqlite3.connect('./db.sqlite3') as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute(
-            """
+        db_cursor.execute("""
         Insert into Users (
             first_name, 
             last_name, 
@@ -60,22 +60,22 @@ def create_user(user_request_body):
             active
         ) 
         values (?, ?, ?, ?, ?, ?, ?, 1)
-        """,
-            (
-                user_request_body["first_name"],
-                user_request_body["last_name"],
-                user_request_body["username"],
-                user_request_body["email"],
-                user_request_body["password"],
-                user_request_body["bio"],
-                datetime.now(),
-            ),
-        )
+        """, (
+            user_request_body['first_name'],
+            user_request_body['last_name'],
+            user_request_body['username'],
+            user_request_body['email'],
+            user_request_body['password'],
+            user_request_body['bio'],
+            datetime.now()
+        ))
 
         id = db_cursor.lastrowid
 
-        return json.dumps({"token": id, "valid": True})
-
+        return json.dumps({
+            'token': id,
+            'valid': True
+        })
 
 def update_user(user, body):
     with sqlite3.connect("./db.sqlite3") as conn:
@@ -103,13 +103,12 @@ def update_user(user, body):
   
 def list_users():
     """Lists all users"""
-    with sqlite3.connect("./db.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
+    with sqlite3.connect('./db.sqlite3') as conn:
+        conn.row_factory=sqlite3.Row
+        db_cursor=conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute(
-            """
+        #Write the SQL query to get the information you want
+        db_cursor.execute("""
         SELECT
                           u.id,
                           u.first_name,
@@ -123,53 +122,32 @@ def list_users():
                           u.active
 
             FROM Users u
-            """
-        )
-        query_results = db_cursor.fetchall()
+            """)
+        query_results=db_cursor.fetchall()
 
-        users = []
+        users=[]
         for row in query_results:
             users.append(dict(row))
 
-        serialized_users = json.dumps(users)
+        serialized_users=json.dumps(users)
 
     return serialized_users
-
-
+        
+    
 def retrieve_user(pk):
     """Retrieves a single user by primary key"""
-    with sqlite3.connect("./db.sqlite3") as conn:
+    with sqlite3.connect('./db.sqlite3') as conn: 
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        db_cursor.execute(
-            """
+        db_cursor.execute("""
         SELECT u.id, u.first_name, u.last_name, u.username, u.email, u.bio, u.profile_image_url, u.created_on, u.active
         FROM Users u
         WHERE u.id = ?
-        """,
-            (pk,),
-        )
-
+        """, (pk,))
+        
         user = db_cursor.fetchone()
         dictionary_version_of_object = dict(user) if user else {}
         serialized_user = json.dumps(dictionary_version_of_object)
 
     return serialized_user
-
-
-def delete_user(pk):
-    with sqlite3.connect("./db.sqlite3") as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        # write the SQL query to get the information you want
-        db_cursor.execute(
-            """
-        DELETE FROM Users WHERE id = ?
-                          
-                          """,
-            (pk,),
-        )
-        number_of_rows_delete = db_cursor.rowcount
-    return True if number_of_rows_delete > 0 else False
