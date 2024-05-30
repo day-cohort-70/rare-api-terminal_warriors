aa4119db1d6c06f77cc2d855af68af1ced0845b6
Gary Cason <garrettcason@skiff.com>
2024-05-30 15:50:07 -0500

    addded Tag List Page as well as Create Tag Page

---
+++ views/tag.py
@@ -0,0 +1,42 @@
+import sqlite3
+import json
+from datetime import datetime
+
+
+def create_tag(tag_data):
+    with sqlite3.connect("./db.sqlite3") as conn:
+        db_cursor = conn.cursor()
+        db_cursor.execute(
+            """
+            INSERT INTO TAGS(label)
+            VALUES(?)
+            """,
+            (tag_data["label"],),
+        )
+        id = db_cursor.lastrowid
+        conn.commit()
+        return {"id": id}
+
+
+def list_tags():
+    with sqlite3.connect("./db.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute(
+            """
+        SELECT
+                          t.id,
+                          t.label
+                          FROM Tags t
+"""
+        )
+        query_results = db_cursor.fetchall()
+
+        tags = []
+        for row in query_results:
+            tags.append(dict(row))
+
+        serialized_tags = json.dumps(tags)
+
+    return serialized_tags
