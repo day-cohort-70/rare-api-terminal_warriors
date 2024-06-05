665cf550ac686d2fd86f12db29d4ffb11490dc11
Oliver Kennelly <victoriakennelly@gmail.com>
2024-06-05 16:04:04 -0500

    get functionality to a detailed post supported

--- json-server.py
+++ json-server.py
@@ -5,7 +5,7 @@ from nss_handler import HandleRequests, status
 
 from views import list_users,retrieve_user, create_user,login_user, update_user, delete_user
 from views import list_categories, retrieve_category, create_category,delete_category, update_category
-from views import filteredAllPosts
+from views import filteredAllPosts, postDetails
 from views import create_tag, list_tags,delete_tag,update_tag
 
 
@@ -36,7 +36,10 @@ class JSONServer(HandleRequests):
             response_body = list_tags()
 
         if requested_resource == "posts":
-            response_body = filteredAllPosts(url)
+            if url["pk"] != 0:
+                response_body = postDetails(url["pk"])
+            else: 
+                response_body = filteredAllPosts(url)
 
 
         if response_body == 'id not found':
--- views/__init__.py
+++ views/__init__.py
@@ -1,5 +1,5 @@
 
 from .user import create_user, login_user, update_user, list_users, retrieve_user, delete_user
 from .category import list_categories,retrieve_category,create_category,delete_category, update_category
-from .post import filteredAllPosts
+from .post import filteredAllPosts, postDetails
 from .tag import create_tag, list_tags,delete_tag,update_tag
